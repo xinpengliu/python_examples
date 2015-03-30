b471bdd734df888aa0777b05debc21292e048378
liuxinpeng <danielliu861@gmail.com>
2015-03-30 16:24:55 +0800

    add test4.py

---
+++ test4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+x={'a':'aaa','b':'bbb','c':12}
+print(x['a'])
+print(x['b'])
+print(x['c'])
+
+for key in x:
+	print("key is %s and  value is %s"%(key,x[key]))
+
+
+'''
+知识点：
+将它当成java中的map来用
+
+'''
