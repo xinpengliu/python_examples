45689c876fcf39d10111a6bea63860a29d4d97eb
liuxinpeng <danielliu861@gmail.com>
2015-03-30 16:02:12 +0800

    add test2.py

---
+++ test2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+a=2
+b="test"
+c=str(a)+b
+d="11111"
+e=a+int(d)
+#how to print multiply values
+print("c is %s,e is %i"%(c,e))
+
+'''
+知识点:
+	*用INT和STR函数将字符串和数字进行转换
+	*打印以#开头，而不是习惯的//
+	*打印多个参数的方式
+	'''
